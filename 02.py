6bf66b6005e5252802460632656a4ef35e574f1f
llyyr <llyyr.public@gmail.com>
2023-12-02 11:08:52 +0530

    day 2

---
+++ 02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from aochelper import *
+
+filename = sys.argv[1] if len(sys.argv) > 1 else '02.txt'
+inp = open(filename)
+
+target = {'red': 12, 'green': 13, 'blue': 14}
+p1 = p2 = 0
+for line in inp:
+    vals = {'red': 0, 'green': 0, 'blue': 0}
+    id = line.split(': ')[0].split(' ')[-1]
+    subsets = line.split(': ')[1].split('; ')
+    game = [subset.split(', ') for subset in subsets]
+    ok = True
+    for subset in game:
+        for cube in subset:
+            n, color = cube.split()
+            n = int(n)
+            vals[color] = max(vals[color], n)
+            if n > target[color]:
+                ok = False
+    if ok:
+        p1 += int(id)
+    p2 += vals['red'] * vals['blue'] * vals['green']
+
+print(p1, p2)
--- template.py
+++ template.py
@@ -2,5 +2,5 @@
 
 from aochelper import *
 
-filename = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0].replace('py', 'txt')
+filename = sys.argv[1] if len(sys.argv) > 1 else '00.txt'
 inp = open(filename).read()
