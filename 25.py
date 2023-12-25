752b48b2b2b23831fa4af8e1a9d87274b9a6a15c
llyyr <llyyr.public@gmail.com>
2023-12-25 23:23:44 +0530

    add day 25 one liner

--- 25.py
+++ 25.py
@@ -3,21 +3,27 @@
 from aochelper import *
 
 filename = sys.argv[1] if len(sys.argv) > 1 else '25.txt'
-inp = open(filename)
+inp = open(filename).read().splitlines()
 
-import networkx as nx
-import random
+def orig():
+    import networkx as nx
+    import random
+    nodes = []
+    G = nx.Graph()
+    for line in inp:
+        v, *w = line.replace(':', '').split()
+        nodes.append(v)
+        G.add_edges_from((v, w, {'capacity': 1}) for w in w)
 
-nodes = []
-G = nx.Graph()
-for line in inp:
-    v, *w = line.replace(':', ' ').split()
-    nodes.append(v)
-    G.add_edges_from((v, w, {'capacity': 1}) for w in w)
+    while True:
+        v, w = random.sample(nodes, 2)
+        cut, part = nx.minimum_cut(G, v, w)
+        if cut == 3:
+            print(math.prod(len(p) for p in part))
+            break
 
-while True:
-    v, w = random.sample(nodes, 2) # lol
-    cut, part = nx.minimum_cut(G, v, w)
-    if cut == 3:
-        print(math.prod(len(p) for p in part))
-        break
+def alt():
+    import networkx as n;print(int.__mul__(*map(len,n.spectral_bisection(n.parse_adjlist(map(lambda x:x.replace(':',''),open(0)))))))
+
+orig()
+alt()
