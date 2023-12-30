9c3b2c3b1a6707c7c1c8593d979de91ffa7d681b
llyyr <llyyr.public@gmail.com>
2023-12-31 02:53:27 +0530

    make d17 faster

--- 17.py
+++ 17.py
@@ -6,29 +6,29 @@ filename = sys.argv[1] if len(sys.argv) > 1 else '17.txt'
 G = [list(map(int, l)) for l in open(filename).read().splitlines()]
 R, C = len(G), len(G[0])
 
-def solve(part):
-    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+def solve(min_steps, max_steps):
+    DIRS = ((0, 1), (1, 0), (0, -1), (-1, 0))
     Q = [(0, ((0, 0), 0, 0)), (0, ((0, 0), 1, 0))]
-    seen = set()
+    seen = set(Q)
     while Q:
         dist, state = heapq.heappop(Q)
-        if state in seen:
-            continue
         (r, c), dir, in_dir = state
-        if r == R-1 and c == C-1:
+        if r == R - 1 and c == C - 1:
             return dist
         dirs = []
-        if (in_dir <= 2 and part == 1) or (in_dir <= 9 and part == 2):
-            dirs.append(dir)
-        if part == 1 or (in_dir >= 4 and part == 2):
+        if in_dir >= min_steps:
             dirs.extend([(dir + 1) % 4, (dir + 3) % 4])
+        if in_dir <= max_steps:
+            dirs.append(dir)
         for new_dir in dirs:
             dr, dc = DIRS[new_dir]
             rr, cc = r + dr, c + dc
             if 0 <= rr < R and 0 <= cc < C:
                 new_in_dir = in_dir + 1 if dir == new_dir else 1
                 new_dist = dist + G[rr][cc]
-                heapq.heappush(Q, (new_dist, ((rr, cc), new_dir, new_in_dir)))
-        seen.add(state)
+                new_state = ((rr, cc), new_dir, new_in_dir)
+                if new_state not in seen:
+                    heapq.heappush(Q, (new_dist, new_state))
+                    seen.add(new_state)
 
-print(solve(1), solve(2))
+print(solve(0, 2), solve(4, 9))
